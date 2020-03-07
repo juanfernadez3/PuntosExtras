59a1737ede9fa81e7ffae9da638440fdb87c04fc
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-03-06 21:46:41 -0400

    Se añadio contenido a las carpetas DAL y Entidades

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PuntosExtras.Entidades;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace PuntosExtras.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Data\PersonasDB");
+        }
+
+
+    }
+}
---
+++ Entidades/Personas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PuntosExtras.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string  Nombre { get; set; }
+    }
+}
